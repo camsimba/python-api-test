90508f07c41c28e4fc2ea2694f151a3beaf61abb
camsimba <camsimbeck@gmail.com>
2024-02-27 12:21:03 -0500

    separated NASA api + gitignore

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
+import nasa_api as nasa
 
-
-import requests 
-from PIL import Image 
-
-
-API_KEY = 'BM6A8mdPSJe7jAq4w4He4GX5ttAVgeKRPcoMsrey'
-
-def APOD():
-    params = {"api_key": API_KEY}
-    response = requests.get('https://api.nasa.gov/planetary/apod', params=params)
-    res = response.json()
-    print(res['explanation'])
-    img_url = res['hdurl']
-    im = Image.open(requests.get(img_url, stream=True).raw)
-    im.show()
-
-APOD()
+nasa.APOD()
 
---
+++ nasa_api.py
@@ -0,0 +1,14 @@
+import requests 
+from PIL import Image 
+
+
+API_KEY = 'BM6A8mdPSJe7jAq4w4He4GX5ttAVgeKRPcoMsrey'
+
+def APOD():
+    params = {"api_key": API_KEY}
+    response = requests.get('https://api.nasa.gov/planetary/apod', params=params)
+    res = response.json()
+    print(res['explanation'])
+    img_url = res['hdurl']
+    im = Image.open(requests.get(img_url, stream=True).raw)
+    im.show()
